93505dac98a6f2d152e43d8864ce771639546289
Julio Cezar Dourado <jcd17071997@hotmail.com>
2018-11-07 16:38:16 -0200

    Finalização

--- test.py
+++ test.py
@@ -1,5 +1,6 @@
 import unittest
 import whatsapi
+import getpass
 from selenium import webdriver
 from selenium.webdriver import ChromeOptions, Chrome
 
@@ -11,6 +12,7 @@ app = Flask(__name__)
 opts = ChromeOptions()
 opts.add_argument('--disable-browser-side-navigation')
 opts.add_experimental_option("detach", True)
+opts.add_argument("user-data-dir=C:\\Users\\"+getpass.getuser()+"\\AppData\\Local\\Google\\Chrome\\User Data\\Default")  # this is the directory for the cookies
 
 #driver = WhatsAPIDriver(username="mkhase",client="Chrome")
 
@@ -26,11 +28,11 @@ def sendMessageRoute():
 def iteractOverMessagesRoute():
     return whatsapi.iteractOverMessages(driver)
 
-@app.rou("/getStatusMessage/", methods=['GET','POST'])
-def getStatusMessageRoute:
+@app.route("/getStatusMessage/", methods=['GET','POST'])
+def getStatusMessageRoute():
     return whatsapi.getStatusMessage(driver)
 
 if __name__ == "__main__":
-	app.run(debug=True)
+	app.run(debug=False)
 
 
--- whatsapi.py
+++ whatsapi.py
@@ -7,7 +7,7 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import NoSuchElementException, StaleElementReferenceException
-from datetime import datetime
+from datetime import datetime, timedelta
 from flask import Flask, request, jsonify
 
 saltWhatsAPI = 'DJjashjkdhsauUU2321JKSAo'
@@ -20,7 +20,10 @@ def sendMessage(driver):
             )
 
         content = request.json
+        elem.clear()
         elem.send_keys(content["contactNumber"])
+
+        time.sleep(4)
 
         elem = WebDriverWait(driver, 10).until(
                 EC.presence_of_element_located((By.XPATH,"//*[@id='pane-side']/div/div/div/div[1]"))
@@ -46,6 +49,8 @@ def iteractOverMessages(driver):
     try:
         driver.implicitly_wait(2)
 
+        hashes = []
+
         while EC.presence_of_element_located((By.CLASS_NAME ,"_15G96")):
             elem = WebDriverWait(driver, 3).until(
                     EC.presence_of_element_located((By.CLASS_NAME ,"_15G96"))
@@ -65,7 +70,7 @@ def iteractOverMessages(driver):
             # target = driver.find_element_by_xpath('//span[@title= "{}"]'.format("Amor"))
             # target.click()
 
-            driver.implicitly_wait(8) 
+            time.sleep(4)
 
             client = WebDriverWait(driver, 1).until(
                     EC.presence_of_element_located((By.CLASS_NAME ,"_1f1zm"))
@@ -97,21 +102,23 @@ def iteractOverMessages(driver):
             current_Date = datetime.now()
             formatted_date = current_Date.strftime('%Y-%m-%d %H:%M:%S')
 
-            hashMessage = '{}|||{}'.format(formatted_date,saltWhatsAPI)
+            hashMessage = '{}|||{}|||{}'.format(formatted_date,clientDB[0][0],saltWhatsAPI)
             hashMessage = base64.b64encode(hashMessage.encode()) 
 
             with Database() as db:
                 db.query("INSERT INTO whatsapp_api.messagesSent (hash_message, message, id_client, date_message) VALUES (%s,%s,%s,%s)", (hashMessage, messageToSend, clientDB[0][0], formatted_date))
-
+                hashToJSON = {"name":clientName.text.strip(), "hash": hashMessage.decode("utf-8")}
+                hashes.append(hashToJSON)
 
             try:
                 elem = WebDriverWait(driver, 3).until(
                         EC.presence_of_element_located((By.CLASS_NAME ,"_15G96"))
                     )
+
             except Exception as e:
                 return jsonify({
                         "message":"Mensagem Enviada com Sucesso!", 
-                        "hash": hashMessage.decode("utf-8") 
+                        "hashes": hashes 
                         })
 
 
@@ -166,6 +173,125 @@ def getLastMessageClient(driver,textDirection,id_client):
 
         except (NoSuchElementException, StaleElementReferenceException) as e:
             pass
+
+def getStatusMessage(driver):
+    """
+        Pegar o Status da Mensagem de acordo com o Hash
+    """
+    content = request.json
+
+    try:
+        hashReceived = content["hash"]
+
+        if hashReceived:
+            with Database() as db:
+                query = "SELECT cli.name, ms.message, STR_TO_DATE(ms.date_message, '%Y-%m-%d %H:%i:%S') date_message"
+                query = query + " FROM whatsapp_api.messagesSent ms"
+                query = query + " LEFT JOIN whatsapp_api.clients cli"
+                query = query + " ON cli.id = ms.id_client "
+                query = query + " WHERE"
+                query = query + " hash_message = %s"
+                db.query(query, (hashReceived, ))
+                messageDB = db.fetchall()
+
+                if messageDB:
+                    elem = WebDriverWait(driver, 10).until(
+                        EC.presence_of_element_located((By.XPATH,"//*[@id='side']/div[1]/div/label/input"))
+                    )
+
+                    elem.clear()
+                    elem.send_keys(messageDB[0][0])
+
+                    time.sleep(4)
+
+                    elem = WebDriverWait(driver, 10).until(
+                            EC.presence_of_element_located((By.XPATH,"//*[@id='pane-side']/div/div/div/div[1]"))
+                        )
+
+                    elem.click()
+
+                    SCROLL_PAUSE_TIME = 1.1
+
+                    # Get scroll height
+                    last_height = driver.execute_script("return document.getElementsByClassName('_2nmDZ')[0].scrollHeight")
+
+                    while True:
+                        # Scroll down to bottom
+                        driver.execute_script("document.getElementsByClassName('_2nmDZ')[0].scrollTop = 0;")
+
+                        # Wait to load page
+                        time.sleep(SCROLL_PAUSE_TIME)
+
+                        # Calculate new scroll height and compare with last scroll height
+                        new_height = driver.execute_script("return document.getElementsByClassName('_2nmDZ')[0].scrollHeight")
+                        if new_height == last_height:
+                            break
+                        last_height = new_height
+
+                    messages = WebDriverWait(driver, 10).until(
+                        EC.presence_of_all_elements_located((By.XPATH,"//*[contains(@class, 'message-out')]/div/div/*[contains(@class, '_3zb-j')]"))
+                    )
+
+                    messagesFind = []
+
+                    statusTxt = "Não enviada"
+                    for message in messages:
+                        if (message.text == messageDB[0][1]):
+                            weekday_name = ["SEGUNDA-FEIRA", "TERÇA-FEIRA", "QUARTA-FEIRA", "QUINTA-FEIRA", "SEXTA-FEIRA", "SÁBADO", "DOMINGO"]
+
+                            if (messageDB[0][2].date() == datetime.now().date()):
+                                textDate = "HOJE"
+                            elif (messageDB[0][2].date() == (datetime.now() - timedelta(days=1)).date()):
+                                textDate = "ONTEM"
+                            elif ((messageDB[0][2].date() == (datetime.now() - timedelta(days=2)).date()) or (messageDB[0][2].date() == (datetime.now() - timedelta(days=3)).date()) or (messageDB[0][2].date() == (datetime.now() - timedelta(days=4)).date()) or (messageDB[0][2].date() == (datetime.now() - timedelta(days=5)).date())):
+                                textDate = weekday_name[messageDB[0][2].date().weekday()]
+                            else:
+                                textDate = messageDB[0][2].strftime('%-d/%-m/%Y')
+
+                            blocoData = message.find_element_by_xpath("../../../../../*[contains(@class, '_3rjxZ')]/div/span[text()='{}']".format(textDate))
+
+                            contDivMessages = 1
+                            statusTxt = "Não enviada"
+
+                            while contDivMessages <= len(blocoData.find_elements_by_xpath("../../following-sibling::div")):
+                                try:
+                                    blocoMessage = blocoData.find_element_by_xpath("../../following-sibling::div[position() = {}]/div[contains(@class, 'message-out')]/div/div/*[contains(@class, '_3zb-j')]/span[text()='{}']".format(contDivMessages,message.text))
+
+                                    blocoHour = blocoMessage.find_element_by_xpath("../../../*[contains(@class, '_2f-RV')]/*[contains(@class, '_1DZAH')]/*[contains(@class, '_3EFt_')]")
+
+                                    if blocoHour.text == messageDB[0][2].strftime('%H:%M'):
+                                        
+                                        try:
+                                            blocoStatus = blocoMessage.find_element_by_xpath("../../../*[contains(@class, '_2f-RV')]/*[contains(@class, '_1DZAH')]/*[contains(@class, '_32uRw')]/span[contains(@data-icon,'msg-dblcheck-ack')]")
+                                            statusTxt = "Visualizada"
+                                        except Exception as a:
+                                            try:
+                                                blocoStatus = blocoMessage.find_element_by_xpath("../../../*[contains(@class, '_2f-RV')]/*[contains(@class, '_1DZAH')]/*[contains(@class, '_32uRw')]/span[contains(@data-icon,'msg-dblcheck')]")
+                                                statusTxt = "Enviada"
+                                            except Exception as a:
+                                                try:
+                                                    blocoStatus = blocoMessage.find_element_by_xpath("../../../*[contains(@class, '_2f-RV')]/*[contains(@class, '_1DZAH')]/*[contains(@class, '_32uRw')]/span[contains(@data-icon,'msg-check')]")
+                                                    statusTxt = "Não recebida"
+                                                except Exception as a:
+                                                    statusTxt = "Não enviada"
+
+                                        break
+                                        
+                                    contDivMessages = contDivMessages + 1
+
+                                except Exception as e:
+                                    #print (e)
+                                    contDivMessages = contDivMessages + 1
+
+                            break
+
+                    formatted_date = messageDB[0][2].strftime('%Y-%m-%d %H:%M:%S')
+                    return jsonify({"name_client": messageDB[0][0], "message": messageDB[0][1],"date_message": formatted_date, "status": statusTxt})
+
+    except Exception as e:
+        raise e
+
+    return jsonify({"message":"Parâmetros incorretos"})
 
 def iteractOverLastMessage(driver,message):
     """
